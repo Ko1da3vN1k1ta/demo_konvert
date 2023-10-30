f6d3cafdcaf7b81a09923508030cb0de9ed900ea
Ko1da3vN1k1ta <666.shadow.999@mail.ru>
2023-10-30 15:23:48 +0300

    allInclusive

--- src/main/java/com/example/demo_konvert/HelloController.java
+++ src/main/java/com/example/demo_konvert/HelloController.java
@@ -1,15 +1,66 @@
 package com.example.demo_konvert;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class HelloController {
 
     @FXML
-    private Label welcomeText;
+    private TextField rubleField;
+    @FXML
+    private ComboBox<String> currencyField;
+    @FXML
+    private Button currencyConvert;
+    @FXML
+    private TextField currencyFinalField;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+private void initialize(){
+        currencyField.getItems().addAll("USD", "EUR", "GBR", "JPY", "CHF");
+        currencyField.setValue("USD");
+
+        currencyConvert.setOnAction(event -> currencyConvertation());
+    }
+    private void currencyConvertation(){
+
+        try {
+            double rubles = Integer.parseInt(rubleField.getText());
+            String selectValue = currencyField.getValue();
+            Random random = new Random();
+            double exchangeRate = random.nextDouble() * (3 - 0.1) + 0.1;
+
+
+            switch (selectValue) {
+                case "USD":
+
+                    currencyFinalField.setText(String.valueOf(exchangeRate));
+                    break;
+                case "EUR":
+                    currencyFinalField.setText(String.valueOf(exchangeRate));
+                    break;
+                case "GBR":
+                    currencyFinalField.setText(String.valueOf(exchangeRate));
+                    break;
+                case "JPY":
+                    currencyFinalField.setText(String.valueOf(exchangeRate));
+
+                    break;
+                case "CHF":
+                    currencyFinalField.setText(String.valueOf(exchangeRate));
+                    break;
+                default:
+                    currencyFinalField.setText("Неверный выбор валюты.");
+            }
+
+        } catch (Exception e) {
+            currencyFinalField.setText("Некорректный ввод. Пожалуйста, введите числовое значение.");
+        }
     }
 }
+
